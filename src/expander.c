301254ba76094c399a0b9e7efa0ec891fa8f2320
Maksim Maksimov <mmaksimo@z1t3c3.42bangkok.com>
2024-11-15 13:24:06 +0700

    style: norme expander.c

--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 17:21:21 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/11/15 00:34:55 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/11/15 13:23:27 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static t_expnd	*alloc_expand(char expanded[], int inquotes[])
 {
-	t_expnd		*expand;
+	t_expnd	*expand;
 
 	if (!expanded || expanded[0] == '\0')
 		return (NULL);
@@ -54,95 +54,18 @@ int	is_expandable_env(char *line_ptr, t_env *envp, char *expanded, int *j)
 	return (env_value - env_entry);
 }
 
-bool non_expandable_env(char *line, int *index, t_env *envp)
-{
-	int offset;
-	size_t len;
-
-	if (!line || !index || !envp || !envp->env)
-		return (false);
-	len = ft_strlen(line);
-	if (*index < 0 || (size_t)*index >= len - 1)
-		return (false);
-	if (!env_exists(&line[*index + 1], *envp->env))
-	{
-		char *start = &line[*index + 1];
-		char *end = start;
-		while (*end && !ft_strchr(" |<>", *end))
-		{
-			if ((size_t)(end - line) >= len - 1)
-				break;
-			end++;
-		}
-		offset = end - start;
-		*index += offset;
-		return (true);
-	}
-	return (false);
-}
-
-// PROBLEM: line can be any size.
-//			need to allocate instead of using fixed-size?
 t_expnd	*expander(char *line, t_env *envp, t_quote *quote)
 {
-	char	expanded[PATH_MAX];
-	int		inqoutes[PATH_MAX];
-	int		j;
-	int		i;
+	t_expd_data		data;
+	char			expanded[PATH_MAX];
+	int				inquotes[PATH_MAX];
 
 	if (!line || !envp || !quote)
 		return (NULL);
-	ft_memset(inqoutes, 0, PATH_MAX);
-	i = 0;
-	j = 0;
-	while (line[i])
-	{
-		if (dequoter(&line[i], quote))
-		{
-			i++;
-			continue ;
-		}
-		if (quote->isquoted)
-			inqoutes[j] = 1;
-		if (line[i] == '$' && !quote->sngl_quote)
-		{
-			if (line[i + 1] == '\0' || line[i + 1] == '\"')
-				expanded[j] = '$';
-			else if (line[i + 1] == '?')
-			{
-				expanded[j] = '\0';
-				char *last_exit_code = ft_itoa(envp->exit_code);
-				if (!last_exit_code)
-					return (0);
-				j += mini_strcat(&expanded[j], last_exit_code);
-				i += 2;
-				free(last_exit_code);
-				continue ;
-			}
-			else if (env_exists(&line[i + 1], *envp->env))
-			{
-				i += is_expandable_env(&line[i + 1], envp, &expanded[j], &j);
-				continue ;
-			}
-			else if (ft_strchr(&line[i + 1], '$'))
-			{
-				i += ft_strchr(&line[i + 1], '$') - line;
-				continue ;
-			}
-			else
-			{
-				i++;
-				while (line[i] && !ft_strchr(" |<>\"'$", line[i]))
-					i++;
-				continue;
-			}
-		}
-		else
-			expanded[j] = line[i];
-		i++;
-		j++;
-	}
-	expanded[j] = '\0';
-	inqoutes[j] = -1;
-	return (alloc_expand(expanded, inqoutes));
+	ft_memset(inquotes, 0, PATH_MAX);
+	data.expanded = expanded;
+	data.inquotes = inquotes;
+	init_expand(&data, line, envp, quote);
+	process_line(&data);
+	return (alloc_expand(expanded, inquotes));
 }
